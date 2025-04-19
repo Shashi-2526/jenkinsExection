594fbd03619f1067eb58b36fe52a054b5e41ced7
hp <hp@LAPTOP-MPMJTA5B>
2025-04-19 10:45:47 +0530

    First commit

---
+++ Myproject/src/test/java/jenkins/JenkinsTest.java
@@ -0,0 +1,22 @@
+package jenkins;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.model.Report;
+
+public class JenkinsTest {
+@Test
+public void lounch() {
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	driver.get("https://demowebshop.tricentis.com/");
+	Reporter.log("DemowebShop lunch Successfully",true);
+	
+}
+}
